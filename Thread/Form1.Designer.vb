0893e516592f7306c072aaff90cc5dbe9f165f5b
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-26 19:54:41 -0500

    Thread example

---
+++ Thread/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnThread = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnThread
+        '
+        Me.btnThread.Location = New System.Drawing.Point(33, 79)
+        Me.btnThread.Name = "btnThread"
+        Me.btnThread.Size = New System.Drawing.Size(213, 23)
+        Me.btnThread.TabIndex = 0
+        Me.btnThread.Text = "Start New Thread"
+        Me.btnThread.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 194)
+        Me.Controls.Add(Me.btnThread)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnThread As Button
+End Class
---
+++ Thread/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private myThread As System.Threading.Thread
+
+    Private Sub btnThread_Click(sender As Object, e As EventArgs) Handles btnThread.Click
+        myThread = New System.Threading.Thread(AddressOf showMessage)
+        myThread.Start()
+    End Sub
+
+    Private Sub showMessage()
+        MessageBox.Show("I am on thread myThread")
+
+    End Sub
+End Class
